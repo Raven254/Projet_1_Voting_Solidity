e7cb25b595d363790c0332c501a1b9cd0605a379
0xMeurwo <marwaane.eljaafari@gmail.com>
2022-06-14 19:25:03 +0200

    réglage de bugs + ajouts divers et consolidations

--- Voting.sol
+++ Voting.sol
@@ -41,26 +41,35 @@ uint voteCount;
 
 // Définition des mappings
 mapping(address => Voter) public votersID; // Détail des électeurs par adresse
-mapping(address => Proposal) public proposalsID; //Détail des propositions par adresse
+mapping(address => Proposal) public proposalsID; // Détail des propositions par adresse
 
 // Tableau non fini répertoriant les propositions
 Proposal[] public proposals;
 
-// ID de la proposition (incrementé dans une fonction pour chaque proposition)
-uint public proposalId = 0; // Peut être appelé pour savoir le nombre de propositions
+// ID de la proposition (incrémenté dans une fonction pour chaque proposition)
+uint private proposalId = 0; // Je mets private pour ne pas que ce soit consultable, pour éviter les confusions
 
 // ID de la proposition gagnante
 uint public winningProposalId = 0;
 
-// Fonction pour retrouver le nombre de propositions
+// FONCTIONS D'AIDE GLOBALES
+
+// 1 - Fonction pour retrouver le nombre de propositions
 function proposalsNumber() public returns(uint){
     return proposals.length+1;
 }
 
-// Définition du statut par défaut du workflow : enregistrement des électeurs
-WorkflowStatus public statut = WorkflowStatus.RegisteringVoters;
+// 2 - Fonction pour observer les différentes propositions
+function seeProposalWith_ID(uint _id) external view returns(string) {
+    return proposals[_id].description;
+}
 
-// Fonction pour retourner à l'étape d'enregistrement des électeurs et tout recommencer
+// 3 - Fonction pour retrouver la proposition d'une adresse --> [UTILITE?]
+function seeProposalWith_Addr(address _address) external view returns(string, uint) {
+    return (proposalsID[_address].description, proposalsID[_address].voteCount);
+}
+
+// 4 - Fonction pour retourner à l'étape d'enregistrement des électeurs et tout recommencer
 // function à faire dans un second temps s'il me reste du temps
 
 
@@ -69,11 +78,10 @@ WorkflowStatus public statut = WorkflowStatus.RegisteringVoters;
 //-----------------------------------------------------------
 
 // PHASE 0 : Enregistrement des électeurs
-function whitelist(address _address) external onlyOwner {
-    require(votersID[_address].isRegistered == false);
-}
+// 1 - Définition du statut par défaut du workflow : enregistrement des électeurs
+WorkflowStatus public statut = WorkflowStatus.RegisteringVoters;
 
-// PHASE 0 : Enregistrement des électeurs
+// 2 - Whitelisting d'adresses par l'administrateur du vote
 function whitelist(address _address) external onlyOwner {
     require(votersID[_address].isRegistered == false);
     require(statut == WorkflowStatus.RegisteringVoters);
@@ -97,14 +105,12 @@ function startingRegistration() external onlyOwner {
 function registration(string _description) external {
     require(statut == WorkflowStatus.ProposalsRegistrationStarted);
     require(votersID[msg.sender].isRegistered == true);
+    proposalsID[msg.sender] = Proposal(_description, 0);
     Proposal memory proposal = Proposal(_description, 0);
     proposals.push(proposal);
     emit ProposalRegistered(proposalId);
     proposalId += 1;
 }
-
-// *FONCTION ENREGISTREMENT*
-// require whitelisté et le bon state
 
 // 3 - Fin de la phase d'enregistrement
 function endingRegistration() external onlyOwner {
@@ -125,9 +131,12 @@ function startingVote() external onlyOwner {
 }
 
 // 2 - Enregistrement des votes
-
-// *FONCTION VOTES*
-// require whitelisté et le bon state
+function registration(_proposalId) external {
+    require(statut == WorkflowStatus.VotingSessionStarted);
+    require(votersID[msg.sender].isRegistered == true);
+    
+    emit Voted(msg.sender, _proposalId); // Envoie l'info du vote à l'interface
+}
 
 // 3 - Fin de la phase de vote
 function endingVote() external onlyOwner {
@@ -146,12 +155,11 @@ function endingVote() external onlyOwner {
 // require le bon state + onlyOwner
 // Renseigne l'uint winningProposalId, ou alors placer la fonction getWinner
 
-// 3 - Fin de la phase de comptabilisation
+// 2 - Fin de la phase de comptabilisation
 function voteTallied() external onlyOwner {
     require(statut == WorkflowStatus.VotingSessionEnded);
     statut = WorkflowStatus.VotesTallied;
     emit WorkflowStatusChange(WorkflowStatus.VotingSessionEnded, WorkflowStatus.VotesTallied); // Envoie l'info à l'interface que le statut a changé
 }
-
 
 }
