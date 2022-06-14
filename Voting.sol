8a6f6a244dedb51ebf7447eae0b9ba1fb5b64ca3
0xMeurwo <marwaane.eljaafari@gmail.com>
2022-06-15 01:16:48 +0200

    Modifications multiples et corrections après tests sur Remix IDE

--- Voting.sol
+++ Voting.sol
@@ -42,6 +42,7 @@ uint voteCount;
 // Définition des mappings
 mapping(address => Voter) public votersID; // Détail des électeurs par adresse
 mapping(address => Proposal) public proposalsID; // Détail des propositions par adresse
+mapping(address => bool) public suiviProposition; // Permet de suivre qui a soumis une proposition
 
 // Tableau non fini répertoriant les propositions
 Proposal[] public proposals;
@@ -52,11 +53,14 @@ uint private proposalId = 0; // Je mets private pour ne pas que ce soit consulta
 // ID de la proposition gagnante
 uint public winningProposalId = 0;
 
+// Nombre d'électeurs
+uint public votersNumber = 0;
+
 // FONCTIONS D'AIDE GLOBALES
 
 // 1 - Fonction pour retrouver le nombre de propositions
 function proposalsNumber() public view returns(uint){
-    return proposals.length+1;
+    return proposals.length;
 }
 
 // 2 - Fonction pour observer les différentes propositions avec l'ID de celles-ci
@@ -78,14 +82,22 @@ function seeProposalWith_Addr(address _address) external view returns(string mem
 //-----------------------------------------------------------
 
 // PHASE 0 : Enregistrement des électeurs
-// 1 - Définition du statut par défaut du workflow : enregistrement des électeurs
+// 1.a - Définition du statut par défaut du workflow : enregistrement des électeurs
 WorkflowStatus public statut = WorkflowStatus.RegisteringVoters;
+
+// 1.b - Fonction pour revenir à RegisteringVoters, au cas où
+function startingRegisteringVoters() external onlyOwner {
+    require(statut != WorkflowStatus.RegisteringVoters, unicode"Vous êtes déjà en phase d'enregistrement des électeurs.");
+    emit WorkflowStatusChange(statut, WorkflowStatus.RegisteringVoters); // Envoie l'info à l'interface que le statut a changé
+    statut = WorkflowStatus.RegisteringVoters;
+}
 
 // 2 - Whitelisting d'adresses par l'administrateur du vote
 function whitelist(address _address) external onlyOwner {
     require(statut == WorkflowStatus.RegisteringVoters, unicode"Erreur : vous n'êtes pas à l'étape RegisteringVoters. Vérifiez le [statut] du workflow.");
     require(votersID[_address].isRegistered == false, unicode"L'électeur est déjà enregistré.");
     votersID[_address].isRegistered = true;
+    votersNumber += 1;
     emit VoterRegistered(_address); // Envoie l'info à l'interface que l'électeur est enregistré
 }
 
@@ -94,7 +106,7 @@ function whitelist(address _address) external onlyOwner {
 // PHASE 1 : Enregistrement des propositions
 
 // 1 - Changement de phase --> Enregistrement des propositions
-function startingRegistration() external onlyOwner {
+function startingProposalsRegistration() external onlyOwner {
     require(statut == WorkflowStatus.RegisteringVoters, unicode"Erreur : vous n'êtes pas à l'étape RegisteringVoters. Vérifiez le [statut] du workflow.");
     statut = WorkflowStatus.ProposalsRegistrationStarted;
     emit WorkflowStatusChange(WorkflowStatus.RegisteringVoters, WorkflowStatus.ProposalsRegistrationStarted); // Envoie l'info à l'interface que le statut a changé
@@ -102,19 +114,20 @@ function startingRegistration() external onlyOwner {
 
 // 2 - Enregistrement des propositions
 
-function proposalRegistration(string calldata _description) external {
+function proposalsRegistration(string calldata _description) external {
     require(statut == WorkflowStatus.ProposalsRegistrationStarted, unicode"Erreur : l'enregistrement des propositions n'a pas encore démarré / est terminée. Vérifiez le [statut] du workflow.");
     require(votersID[msg.sender].isRegistered == true, unicode"Erreur : vous n'êtes pas enregistré pour voter.");
-    require(!proposalsID[msg.sender], unicode"Erreur : vous avez déjà soumis une proposition.");
+    require(!suiviProposition[msg.sender], unicode"Erreur : vous avez déjà soumis une proposition.");
     proposalsID[msg.sender] = Proposal(_description, 0);
     Proposal memory proposal = Proposal(_description, 0);
     proposals.push(proposal);
     emit ProposalRegistered(proposalId);
+    suiviProposition[msg.sender] = true;
     proposalId += 1;
 }
 
 // 3 - Fin de la phase d'enregistrement
-function endingRegistration() external onlyOwner {
+function endingProposalsRegistration() external onlyOwner {
     require(statut == WorkflowStatus.ProposalsRegistrationStarted, unicode"Erreur : vous n'êtes pas à l'étape ProposalRegistrationStarted. Vérifiez le [statut] du workflow.");
     statut = WorkflowStatus.ProposalsRegistrationEnded;
     emit WorkflowStatusChange(WorkflowStatus.ProposalsRegistrationStarted, WorkflowStatus.ProposalsRegistrationEnded); // Envoie l'info à l'interface que le statut a changé
@@ -158,7 +171,8 @@ function endingVote() external onlyOwner {
 // PHASE 3 : Phase de dépouillement des votes
 
 // 1 - Comptabilisation des votes
-function voteCounting() private onlyOwner returns(uint) {
+function voteCounting() external onlyOwner returns(uint) {
+    require(statut == WorkflowStatus.VotingSessionEnded);
     uint IdCount = 0; // On garde cette variable pour suivre la proposition avec le max de votes.
     for(uint i = 1; i <= proposalId - 1; i++) { // Boucle pour trouver le maximum, en comparant la clé i-1 avec la clé i.
         if (proposals[i].voteCount > proposals[i-1].voteCount){ // Cela peut occasionner un bug si 2 comptes sont égaux... Voir comment gérer
